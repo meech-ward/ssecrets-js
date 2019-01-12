492a6143dc038c9b1cc6f90db6353fe4307eb625
Sam Meech-Ward <sam@meech-ward.com>
2019-01-12 08:37:24 -0800

    add documentation to hash

--- src/hash.js
+++ src/hash.js
@@ -8,16 +8,30 @@ const compare = R.curry(function(saltRounds, text, hash, cb) {
     cb(null, false);
     return;
   }
-  cb(null, true);
-  bcryptjs.compare
+  bcryptjs.compare(text, hash, cb);
 });
 
+/**
+ * Setup the hash and compare functions with a given number of salt rounds. 
+ * All compares must have hashes with that number of salt rounds.
+ */
 module.exports = function(saltRounds = 12) {
   const hash = R.curry(bcryptjs.hash);
-  const hash12 = hash(R.__, saltRounds);
+  const hashRounds = hash(R.__, saltRounds);
 
   return {
-    hash: hash12,
+    /**
+     * Hash some text.
+     * @param text the plain text to hash.
+     * @param cb (error, hash)
+     */
+    hash: hashRounds,
+    /**
+     * Compare a hash and plain text.
+     * @param text the plain text to compare it to
+     * @param hash the hash to compare to
+     * @param cb (error, match)
+     */
     compare: compare(saltRounds)
   };
 }
